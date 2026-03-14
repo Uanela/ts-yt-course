dabbf57f64a4283ea1a8cf185225f671365d47ec
uanela <uanelaluiswayne@gmail.com>
2026-03-14 21:19:05 +0200

    3-primitivos-basicos

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-let firstName = "Uanela Como";
-
+let firstName: string | number = "Uanela Como";
 firstName = "Bede";
+firstName = 2;
 
-console.log(firstName);
+const age: number = 40;
+const isActive: boolean = true;
+const address: undefined = undefined;
+
+console.log(firstName, age);
+// string, number, boolean, null
+
+const firstNames: (string | number | boolean)[] = ["Franciso", 2];
+
+firstNames.push(true);
+
+console.log(firstNames);

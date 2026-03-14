a914b1736d44a1506f7ee7a2542cd2dfb7a9a323
uanela <uanelaluiswayne@gmail.com>
2026-03-14 21:30:29 +0200

    4-inferencia-de-tipos

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,29 @@
-let firstName: string | number = "Uanela Como";
-firstName = "Bede";
-firstName = 2;
+// let firstName: string | number = "Uanela Como";
+// firstName = "Bede";
+// firstName = 2;
 
-const age: number = 40;
-const isActive: boolean = true;
-const address: undefined = undefined;
+// const age: number = 40;
+// const isActive: boolean = true;
+// const address: undefined = undefined;
 
-console.log(firstName, age);
-// string, number, boolean, null
+// console.log(firstName, age);
+// // string, number, boolean, null
 
-const firstNames: (string | number | boolean)[] = ["Franciso", 2];
+// const firstNames: (string | number | boolean)[] = ["Franciso", 2];
 
-firstNames.push(true);
+// firstNames.push(true);
+// console.log(firstNames);
 
-console.log(firstNames);
+let car: string = "BWM";
+let brand: number = 3;
+
+function getAge(year: number): number | string {
+  if (year > 2000) return 2 + 2;
+  else return "hello";
+}
+
+let age = getAge(200);
+
+age = [];
+
+console.log(age);

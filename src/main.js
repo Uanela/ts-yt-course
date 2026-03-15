cd72a251efb8fd03ad498292c40e9ddcb4536f10
uanela <uanelaluiswayne@gmail.com>
2026-03-15 15:52:00 +0200

    6-typescript-e-html

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ let firstName = "Uanela Como";
 firstName = 2;
 
 console.log(firstName);
+
+let person;
+
+// console.log(person.name);
+
+function sum(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(sum(4, "uanela"));
+
+console.log(laptop);
---
+++ src/scripts/index.js
@@ -0,0 +1,39 @@
+// let firstName: string | number = "Uanela Como";
+// firstName = "Bede";
+// firstName = 2;
+// const age: number = 40;
+// const isActive: boolean = true;
+// const address: undefined = undefined;
+// console.log(firstName, age);
+// // string, number, boolean, null
+// const firstNames: (string | number | boolean)[] = ["Franciso", 2];
+// firstNames.push(true);
+// console.log(firstNames);
+let car = "BWM";
+let brand = 3;
+// brand = false;
+function getAge(year) {
+  if (year > 2000) return 2 + 2;
+  else return "hello";
+}
+let age = getAge(200);
+age = 2;
+console.log(age);
+// Os Error mais comuns
+// 1 - Type x is not assignable to type y
+// 2 - Property x does not exist on type y
+const person = { name: "Uanela", age: 2 };
+console.log(person.age);
+// 3 - Object is possibly null or undefined
+let product;
+if (product) console.log(product.name);
+// 4 - Argument of type x is not assignable to parameter of type y
+function sum(num1, num2) {
+  return num1 * num2;
+}
+///@ts-ignore
+console.log(sum(4, ":"));
+// Cannot find name x
+///@ts-ignore
+// console.log(laptop);
+console.log("Hello World From TypeScript");
